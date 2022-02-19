a5982e4eb264c696c07074ba660ae4401ef02f7c
Ryan Maloney <rypmaloney@gmail.com>
2022-02-19 13:29:56 -0500

    Created layout to mimic Wabi Cycles homepage.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Wabi Cycles Inventory' });
 });
 
 module.exports = router;
