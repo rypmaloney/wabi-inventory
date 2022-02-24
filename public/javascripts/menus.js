2cd79ad31595cc37d83d4b8b25182406001849c3
Ryan Maloney <rypmaloney@gmail.com>
2022-02-24 14:49:11 -0500

    Complete overhaul of menu styling. Added functionality for mobile menu. Added client side JS to operate mobile menu.

---
+++ public/javascripts/menus.js
@@ -0,0 +1,36 @@
+
+function menuController(){
+    (function(){
+        window.addEventListener('mouseover', clickAway);
+        function clickAway(e){
+            if (e.target.matches('.main')){
+                let dropdown = document.getElementsByClassName('menu-item');
+                for(let i=0; i<dropdown.length; i++){
+                    if (dropdown[i].classList.contains('showMobile')){
+                        dropdown[i].classList.remove('showMobile');
+                    }
+                }
+            }
+        }
+    })();
+
+    //Mobile menu expand
+    (function(){
+        let menuBtn = document.getElementById('menu');
+
+        menuBtn.addEventListener('click', () => expandMenu())
+
+
+        function expandMenu(){
+            
+            let menuItems = document.getElementsByClassName('menu-item');
+
+            Array.from(menuItems).forEach(element => {
+            element.classList.toggle('showMobile');
+            })
+    }
+    })();
+}
+
+menuController()
+
